5cc7cd483debe7cb46f239aa88aa4dce70c2e024
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2022-10-16 10:42:20 +0100

    But fixes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,7 @@
-#This command will create a file
-#!/usr/bin/pup
-file{'/tmp/school':
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet'
+# Creates a file
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
-
-
