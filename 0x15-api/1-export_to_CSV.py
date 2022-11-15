08727c9747775e3c91a6917eea17d9c1008cdcfc
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2022-11-15 13:04:39 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,11 +9,11 @@ import sys
 if __name__ == '__main__':
     endpoint = "https://jsonplaceholder.typicode.com/"
     userId = sys.argv[1]
-    user = requests.get(endpoint + 'users/{}'.format(userId)).json()
-    todo = requests.get(endpoint + 'todos?userId={}'.format(userId)).json()
+    user = requests.get(endpoint + 'users/{}'.format(USER_ID)).json()
+    todo = requests.get(endpoint + 'todos?userId={}'.format(USER_ID)).json()
 
-    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
+    with open("{}.csv".format(USER_ID), 'w', newline='') as csvfile:
         write_to_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todo:
-            write_to_file.writerow([int(userId), user.get(
-                'username'), task.get('completed'), task.get('title')])
+            write_to_file.writerow([int(USER_ID), user.get(
+                'USERNAME'), task.get('TASK_COMPLETED_STATUS'), task.get('TASK_TITLE')])
