35501d1329949b323fe2ca52c3f4a9f1a173f46a
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2022-11-15 13:05:52 +0100

    Corrections

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,4 +16,5 @@ if __name__ == '__main__':
         write_to_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todo:
             write_to_file.writerow([int(USER_ID), user.get(
-                'USERNAME'), task.get('TASK_COMPLETED_STATUS'), task.get('TASK_TITLE')])
+                'USERNAME'), task.get('TASK_COMPLETED_STATUS'), 
+                task.get('TASK_TITLE')])
