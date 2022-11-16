d93729b75f4ae9859178c07fcd90cc46d385a1d4
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2022-11-16 23:32:20 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Export data into the CSV format"""
-
+"""
+extend your Python script to export data in the CSV format
+"""
 
 import csv
 import requests
-import sys
+from sys import argv
 
 if __name__ == '__main__':
     endpoint = "https://jsonplaceholder.typicode.com/"
-    USER_ID = sys.argv[1]
-    user = requests.get(endpoint + 'users/{}'.format(USER_ID)).json()
-    todo = requests.get(endpoint + 'todos?userId={}'.format(USER_ID)).json()
-
-    with open("{}.csv".format(USER_ID), 'w', newline='') as csvfile:
-        write_to_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    userId = argv[1]
+    user = requests.get(endpoint + "users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get(endpoint + "todos?userId={}".
+                        format(userId), verify=False).json()
+    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
+        my_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todo:
-            write_to_file.writerow([int(USER_ID), user.get(
-            'USERNAME'), task.get('TASK_COMPLETED_STATUS'), 
-task.get('TASK_TITLE')])
+            my_writer.writerow([int(userId), user.get('username'),
+                                task.get('completed'),
+                                task.get('title')])
