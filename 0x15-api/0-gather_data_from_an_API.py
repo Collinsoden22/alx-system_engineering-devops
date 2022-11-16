5450393c0f24478d5f1865717b417555fe9c7967
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2022-11-16 19:27:41 +0100

    Update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
-"""Getting data from API placeholder."""
+""" Use API REST https://jsonplaceholder.typicode.com"""
 
-import requests
-import sys
+from requests import get
+from sys import argv
 
-if __name__ == '__main__':
-    """Gets API endpoint, then identify a
- user to display completed task info"""
-    endpoint = "https://jsonplaceholder.typicode.com/"
-    userId = sys.argv[1]
-    user = requests.get(endpoint + 'users/{}'.format(userId)).json()
-    todo = requests.get(endpoint + 'todos?userId={}'.format(userId)).json()
-    completed = []
 
-    for task in todo:
-        if task.get("completed"):
-            completed.append(task.get("TASK_TITLE"))
-    print("Employee {} is done with task({}/{}):"
-          .format(user.get('EMPLOYEE_NAME'), len(completed), len(todo)))
-    for task in completed:
+def tasks_list(employee_id=None):
+    """ Return employee info TODO list """
+
+    API = "https://jsonplaceholder.typicode.com/"
+
+    try:
+        employee_id = int(employee_id)
+    except:
+        return "No valid employee id"
+
+    employee = get(API + "users/{}". format(employee_id)).json()
+    todos = get(API + "todos?userId={}".format(employee_id)).json()
+
+    name = employee.get("name")
+    tasks_total = len(todos)
+    tasks_done = 0
+    titles_tasks = []
+
+    for task in todos:
+        if task.get("completed") is True:
+            titles_tasks.append(task.get("title"))
+            tasks_done = tasks_done + 1
+
+    del todos, employee
+
+    print("Employee {:s} is done with tasks({:d}/{:d}):".
+          format(name, tasks_done, tasks_total))
+
+    for task in titles_tasks:
         print('\t', task)
+
+
+if __name__ == "__main__":
+    tasks_list(argv[1])
